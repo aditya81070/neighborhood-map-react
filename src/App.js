cc4a3231cc1efd3e0c912ab0321341d00d574a22
Aditya Agarwal <aditya81070@gmail.com>
2018-10-06 07:38:33 +0530

    Add initial google map load condition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { GoogleApiWrapper } from 'google-maps-react'
+// import logo from './logo.svg'
+// import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <div>Initial page render</div>
+          {/* <img src={logo} className='App-logo' alt='logo' /> */}
+          <h1 className='App-title'>Welcome to neighborhood map(React)</h1>
         </header>
+        {
+          this.props.loaded ? (
+            // <Map google={this.props.google} />
+            <div>Google map is loaded</div>
+          ) : (
+            <div className='error-loading'>
+              <p className='error-msg'>Couldn't load Google maps</p>
+            </div>
+          )
+        }
       </div>
     )
   }
 }
 
-export default App
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDYIakkVfbT7wU-6VOAnE9Up10NvVgfwB0'
+})(App)
