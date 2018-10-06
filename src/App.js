2afd3e15c28be3e805445723864dcdc807d5d57d
Aditya Agarwal <aditya81070@gmail.com>
2018-10-06 17:51:42 +0530

    Convert component to function component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { GoogleApiWrapper } from 'google-maps-react'
 import Map from './Components/Map'
 // import logo from './logo.svg'
 // import './App.css'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          {/* <img src={logo} className='App-logo' alt='logo' /> */}
-          <h1 className='App-title'>Welcome to neighborhood map(React)</h1>
-        </header>
-        {
-          this.props.loaded ? (
-            <Map google={this.props.google} />
-          ) : (
-            <div className='error-loading'>
-              <p className='error-msg'>Couldn't load Google maps</p>
-            </div>
-          )
-        }
-      </div>
-    )
-  }
+function App (props) {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <h1 className='App-title'>Welcome to neighborhood map(React)</h1>
+      </header>
+      {
+        props.loaded ? (
+          <Map google={props.google} />
+        ) : (
+          <div className='error-loading'>
+            <p className='error-msg'>Couldn't load Google maps</p>
+          </div>
+        )
+      }
+    </div>
+  )
 }
 
 export default GoogleApiWrapper({
